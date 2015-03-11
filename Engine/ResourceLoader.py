5269522b55ea1519af9795d9e87003f9137ce523
bubbles231 <arthur@info9.net>
2015-03-11 12:17:04 -0500

    Make ResourceLoader Class

---
+++ Engine/ResourceLoader.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+"""
+
+ResourceLoader File
+I will load any and all resources through the class
+in this file.
+"""
+import os
+import pygame
+pygame.mixer.init(8000)  # All sound effects will have this sample rate
+
+if not pygame.font:
+    print('Warning, fonts disabled')  # TODO: Use fonts
+if not pygame.mixer:
+    print('Warning, sound disabled')  # TODO: Use sound
+
+
+class ResourceLoader():
+
+    def __init__(self):
+        pass
+
+    def load_image(self, name, color_key=None):
+        """
+
+        :param name: Name of the image file
+        :param color_key: A pygame option that I included from example online
+        :return: Returns an image and the pygame.Rect() for that image
+        """
+        pathname = os.path.join('Resources/Images/', name)
+        try:
+            image = pygame.image.load(pathname)
+        except pygame.error:
+            print('Cannot load image:', name)
+            print("Quitting!\n")
+            raise SystemExit
+        if color_key is not None:
+            if color_key is -1:
+                color_key = image.get_at((0, 0))
+            image.set_colorkey(color_key, pygame.RLEACCEL)
+        return image, image.get_rect()
+
+    def load_sound(self, name):
+        """
+
+        :param name: Name of the sound file
+        :return: Pygame sound file
+        """
+        class NoneSound:
+            """
+
+            Empty class if system can't use pygame.mixer
+            """
+
+            def __init__(self):
+                pass
+
+            def play(self):
+                """
+
+                Function made for compatibility
+                """
+                pass
+
+        if not pygame.mixer:
+            return NoneSound()
+        fullname = os.path.join('Resources/Sounds/', name)
+        try:
+            sound = pygame.mixer.Sound(fullname)
+        except pygame.error:
+            print('Cannot load sound:', name)
+            print("Quitting!\n")
+            raise SystemExit
+        return sound
--- Main.py
+++ Main.py
@@ -4,34 +4,7 @@
 Main File for Hangman game. This is where all the magic
 starts - Arthur Marble
 """
-import os
-import sys
-import pygame
-
-if not pygame.font:
-    print('Warning, fonts disabled')  # TODO: Use fonts
-if not pygame.mixer:
-    print('Warning, sound disabled')  # TODO: Use sound
-
-
-def load_image(name, color_key=None):
-    """
-    :param name: Name of the image
-    :param color_key: A pygame option that I included from example online
-    :return: Returns an image and the pygame.Rect() for that image
-    """
-    pathname = os.path.join('Resources', name)
-    try:
-        image = pygame.image.load(pathname)
-    except pygame.error:
-        print('Cannot load image:', name)
-        print("Quitting!\n")
-        raise SystemExit
-    if color_key is not None:
-        if color_key is -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key, pygame.RLEACCEL)
-    return image, image.rect()
+from Engine.ResourceLoader import *
 
 
 def main():
@@ -41,8 +14,23 @@ def main():
     """
 
     pygame.init()
-    print("Make Splash Screen!...")
+    splash, splash_rect = ResourceLoader().load_image('Splash1.png')
+    sound = ResourceLoader().load_sound('Splash_Sound1.wav')
+    screen = pygame.display.set_mode((640, 400))
+    pygame.display.set_caption("Hangman Clone!")
+    playing = True
+    while playing:
+        # Main Game Loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                sound.play()
+        screen.blit(splash, splash_rect)
+        pygame.display.flip()
+        pygame.time.Clock().tick(60)  # TODO: MAKE FPS ADJUSTABLE
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
