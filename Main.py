836efdacbafb00ce9b62e13b54ad28da9a14f805
bubbles231 <arthur@info9.net>
2015-03-11 10:12:46 -0500

    first commit

---
+++ Main.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+
+Main File for Hangman game. This is where all the magic
+starts - Arthur Marble
+"""
+import pygame
+
+
+def main():
+    """
+    Start with a splash screen.
+    :return:
+    """
+    pygame.init()
+    print("Make Splash Screen!...")
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
